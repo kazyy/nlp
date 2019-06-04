6571e199f0623982477356ae35230d5d61d97318
kazyy <hideki.kajiwara@gmail.com>
2019-06-04 19:30:27 +0900

    fix

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,10 @@ RUN ln -sf /usr/share/zoneinfo/Japan /etc/localtime
 
 RUN yum install -y https://centos6.iuscommunity.org/ius-release.rpm
 RUN yum install -y python36*
+RUN pip3.6 install --upgrade pip
+RUN pip3.6 install requests
+RUN pip3.6 install beautifulsoup4
 
 COPY src/* /nlp/
 
-ENTRYPOINT python3.6 /nlp/test.py >> /nlp/log/log.txt
+ENTRYPOINT /bin/bash
--- src/test.py
+++ src/test.py
@@ -1,1 +1,24 @@
-print('start')
+import requests
+import re
+from bs4 import BeautifulSoup
+from re import RegexFlag
+from builtins import exit
+
+
+res = requests.get('https://www.tennoji-mio.co.jp/products/')
+m = re.search('<body(.*?)>(.+)</body>', res.text, flags = RegexFlag.MULTILINE|RegexFlag.DOTALL)
+soup = BeautifulSoup(m.group(2), 'html.parser')
+links = soup.select('ul.shop_list li a')
+if not links:
+    print('HTML構造エラー')
+    exit
+
+for a in links:
+    r = requests.get(a['href'])
+    s = BeautifulSoup(r.text, 'html.parser')
+
+    if not s.body.article:
+        print('HTML構造エラー')
+        continue
+
+    print(s.body.article.get_text())
